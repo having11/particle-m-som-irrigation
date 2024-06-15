e584795863e3228d8c0c39c715c2df268be83c41
having11 <gatoninja236@gmail.com>
2024-06-14 22:19:20 -0500

    watering algo

--- web/msom-irrigation/server/plugins/process-forecast.ts
+++ web/msom-irrigation/server/plugins/process-forecast.ts
@@ -1,5 +1,6 @@
 import { NWSForecastOfficeId, gridpointForecast } from "~/integrations/weather";
 import { getLatestSoilValues } from "../utils/db";
+import { setValveState } from "../utils/device";
 
 export default defineNitroPlugin(nitroApp => {
   setInterval(async () => {
@@ -17,6 +18,30 @@ export default defineNitroPlugin(nitroApp => {
     // Read latest soil % from db
     const values = await getLatestSoilValues();
 
-    // TODO: Call watering algo
+    // Call watering algo
+    if (nextPrecip && shouldWater(nextPrecip, values.map(v => v.value))) {
+      for (var i = 0; i < 4; i++) {
+        await setValveState(i, true);
+      }
+    }
   }, 5 * 60 * 1000);
 });
+
+export const shouldWater = (precipChance: number, soilValues: number[]): boolean => {
+  // < 55% soil moisture
+  const soilThreshold = 0.55;
+  // >= 60% precip chance
+  const precipThreshold = 0.6;
+
+  // Don't water if rain is coming
+  if (precipChance >= precipThreshold) {
+    return false;
+  }
+
+  // Water if soil is too dry
+  if (soilValues.reduce((prev, cur) => prev + cur) / soilValues.length < soilThreshold) {
+    return true;
+  }
+
+  return false;
+}
